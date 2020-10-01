8ddf8139108f41349be55ec7800fb38b003497f0
quentincaffeino <sergei.zhuravlev@abuco.cz>
2020-10-01 12:16:07 +0200

    Closes #5

--- tests/unit/entity/Request.spec.ts
+++ tests/unit/entity/Request.spec.ts
@@ -1,19 +1,19 @@
-jest.mock('axios');
 
+import fetchMock from "jest-fetch-mock"
 import Request from '~/entity/Request';
-import axios from 'axios';
 
 describe('entity/Request', () => {
   it('should execute call and get the response', async () => {
     const data = 'foo';
     const baseUrl = 'bar';
-    (axios.get as jest.Mock).mockResolvedValue({ data });
+    const method = 'get';
+    fetchMock.mockResolvedValue(new Response(JSON.stringify(data)));
 
     const req = new Request(baseUrl);
-    const resp = await req.call('get', '');
+    const resp = await req.call(method, '');
 
     expect(resp).toBe(data);
-    expect(axios.get).toHaveBeenCalledTimes(1);
-    expect(axios.get).toHaveBeenCalledWith(baseUrl);
+    expect(fetchMock).toHaveBeenCalledTimes(1);
+    expect(fetchMock).toHaveBeenCalledWith(baseUrl, { method });
   });
 });
